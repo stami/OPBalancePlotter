5b1912f193a1f330f4a0a239c5ec1fefad8c08a7
Samuli Tamminen <samuli.tamminen@vincit.com>
2018-01-10 22:15:17 +0200

    show transactions in the tooltip

--- scripts.js
+++ scripts.js
@@ -35,6 +35,7 @@ function handlePlotButtonClick(event) {
 
   initialBalance = parseFloat($("#initialBalance").val() || "0");
   console.log("initial balance", initialBalance);
+  console.log("parsed data", data);
 
   if (data) {
     const dataForPlot = balanceDataForPlot(data);
@@ -57,12 +58,14 @@ function balanceDataForPlot(data) {
 
     const dateKey = data.meta.fields[0]; // "Arvopäivä"
     const deltaKey = data.meta.fields[2]; // "Määrä  EUROA"
+    const recipientKey = data.meta.fields[5]; // "Saaja/Maksaja"
 
     const deltaEur = parseFloat(transaction[deltaKey].replace(",", "."));
 
     return {
       date: transaction[dateKey],
-      delta: deltaEur
+      delta: deltaEur,
+      recipient: transaction[recipientKey]
     };
   });
 
@@ -70,13 +73,20 @@ function balanceDataForPlot(data) {
   const mergedForDays = stripped.reduce((data, transaction) => {
     if (data.length === 0) {
       // First one
-      data.push(transaction);
+      data.push({
+        ...transaction,
+        info: `<b>Transactions:</b> <br>${transaction.delta} €  ${transaction.recipient} <br>`
+      });
     } else {
       const last = data[data.length - 1];
       if (last.date === transaction.date) {
         last.delta += transaction.delta;
+        last.info += `${transaction.delta} €  ${transaction.recipient} <br>`;
       } else {
-        data.push(transaction);
+        data.push({
+          ...transaction,
+          info: `<b>Transactions:</b> <br>${transaction.delta} €  ${transaction.recipient} <br>`
+        });
       }
     }
 
@@ -88,12 +98,14 @@ function balanceDataForPlot(data) {
     if (data.length === 0) {
       data.push({
         date: day.date,
-        balance: day.delta
+        balance: day.delta,
+        info: day.info
       });
     } else {
       const last = data[data.length - 1];
       data.push({
         date: day.date,
+        info: day.info,
         balance: last.balance + day.delta
       })
     }
@@ -104,7 +116,7 @@ function balanceDataForPlot(data) {
   // Add initial balance
   const withInitialBalance = cumulativeBalance.map((day) => {
     return {
-      date: day.date,
+      ...day,
       balance: day.balance + initialBalance
     };
   });
@@ -114,6 +126,7 @@ function balanceDataForPlot(data) {
     {
       x: withInitialBalance.map((day) => day.date),
       y: withInitialBalance.map((day) => day.balance),
+      text: withInitialBalance.map((day) => day.info),
       type: "scatter"
     }
   ];
